ae3ccd4bbd33469317ea98c5c2a05adbf02781fe
Eric Webber <ewebber283@gmail.com>
2022-07-02 14:50:32 -0500

    added thoughtform

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,6 +4,7 @@ import Auth from '../utils/auth';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 import { useQuery, useMutation } from '@apollo/client';
 
@@ -68,6 +69,7 @@ const Profile = () => {
         />
       </div>
     </div>
+    <div className="mb-3">{!userParam && <ThoughtForm />}</div>
   </div>
   );
 };
